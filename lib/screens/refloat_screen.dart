bc31d25037683a2c1ab8b05864b00b529c3d2456
Nicollas Soares <nicollas-soares@mail.com>
2024-10-12 19:44:59 -0300

    Add LICENSE and updated files

--- lib/screens/refloat_screen.dart
+++ lib/screens/refloat_screen.dart
@@ -1,3 +1,21 @@
+/*
+<DiveCalc is an app to help Public Safety Divers>
+Copyright (C) <2024>  <Nicollas Soares Goncalves>
+
+This program is free software: you can redistribute it and/or modify
+it under the terms of the GNU Affero General Public License as
+published by the Free Software Foundation, either version 3 of the
+License, or (at your option) any later version.
+
+This program is distributed in the hope that it will be useful,
+but WITHOUT ANY WARRANTY; without even the implied warranty of
+MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+GNU Affero General Public License for more details.
+
+You should have received a copy of the GNU Affero General Public License
+along with this program.  If not, see <https://www.gnu.org/licenses/>.
+*/
+
 import 'package:dive_calc2/screens/lifter_refloat_screen.dart';
 import 'package:flutter/material.dart';
 import 'package:dive_calc2/screens/liftbag_refloat_screen.dart';
---
+++ lib/scripts/add_license.dart
@@ -0,0 +1,60 @@
+/*
+<DiveCalc is an app to help Public Safety Divers>
+Copyright (C) <2024>  <Nicollas Soares Goncalves>
+
+This program is free software: you can redistribute it and/or modify
+it under the terms of the GNU Affero General Public License as
+published by the Free Software Foundation, either version 3 of the
+License, or (at your option) any later version.
+
+This program is distributed in the hope that it will be useful,
+but WITHOUT ANY WARRANTY; without even the implied warranty of
+MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+GNU Affero General Public License for more details.
+
+You should have received a copy of the GNU Affero General Public License
+along with this program.  If not, see <https://www.gnu.org/licenses/>.
+*/
+
+import 'dart:io';
+
+void main() {
+  final directory = Directory('lib');
+  final licenseText = '''
+/*
+<DiveCalc is an app to help Public Safety Divers>
+Copyright (C) <2024>  <Nicollas Soares Goncalves>
+
+This program is free software: you can redistribute it and/or modify
+it under the terms of the GNU Affero General Public License as
+published by the Free Software Foundation, either version 3 of the
+License, or (at your option) any later version.
+
+This program is distributed in the hope that it will be useful,
+but WITHOUT ANY WARRANTY; without even the implied warranty of
+MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+GNU Affero General Public License for more details.
+
+You should have received a copy of the GNU Affero General Public License
+along with this program.  If not, see <https://www.gnu.org/licenses/>.
+*/
+
+''';
+
+  if (directory.existsSync()) {
+    directory.listSync(recursive: true).forEach((file) {
+      if (file is File && file.path.endsWith('.dart')) {
+        final content = file.readAsStringSync();
+        if (!content.startsWith(
+            '/*<DiveCalc is an app to help Public Safety Divers>')) {
+          file.writeAsStringSync(licenseText + content);
+          print('License added to ${file.path}');
+        } else {
+          print('License already present in ${file.path}');
+        }
+      }
+    });
+  } else {
+    print('Directory "lib" does not exist.');
+  }
+}
